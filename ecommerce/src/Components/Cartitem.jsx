0a98daa2c634d37956ab8025fd3c2ea26526bcac
walson <walsongodwin@gmail.com>
2022-05-12 19:14:39 +0530

    added heading

--- ecommerce/src/Components/Cartitem.jsx
+++ ecommerce/src/Components/Cartitem.jsx
@@ -1,36 +1,26 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-
-
-
-
-
-
-
-
-export const Mycart=()=>{
-
-
-return (
-
-<>
-
-
-<div>hello</div>
-
-
-
-
-</>
-
-
-
-
-
-)
-
-
-
-
-
+export const  Mycart =()=>  {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stack spacing={2}>
+        <Item>item </Item>
+        <Item>Item </Item>
+        <Item>Item </Item>
+        <h3>This is item</h3>
+      </Stack>
+    </Box>
+  );
 }
